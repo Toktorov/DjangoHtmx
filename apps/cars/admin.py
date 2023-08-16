900d69a95c2e027de19ef79ed90439772835ff7b
Toktorov <ktoktorov2005@gmail.com>
2023-08-16 16:12:48 +0600

    work with post django htmx

--- apps/cars/admin.py
+++ apps/cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.cars.models import Car
+
 # Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('license_plate', 'brand', 'model', 'year')
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from apps.cars.models import Car
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        license_plate = request.POST.get('license_plate').upper()
+        try:
+            car = Car.objects.get(license_plate=license_plate)
+            context = {'car' : car}
+            return render(request, 'index.html', context)
+        except:
+            return render(request, 'index.html')
     return render(request, 'index.html', locals())
 
 def about(request):
